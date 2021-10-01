71e8a785056d72b2e2a0017dc3676571f03eaf44
Fabricio de Freitas <fabricioffa08@gmail.com>
2021-10-01 15:53:37 -0300

    exemplo de seeds

---
+++ src/database/seeds/20211001184058-user-create.js
@@ -0,0 +1,42 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'John Doe',
+          email: 'johndoe@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'John Doe2',
+          email: 'johndoe2@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'John Doe3',
+          email: 'johndoe3@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'John Doe4',
+          email: 'johndoe4@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  down: async () => {},
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,7 @@ import loginRequired from '../middlewares/loginRequired';
 const router = new Router();
 
 // router.post('/', userController.store); //  Não devria existir
-// router.get('/', loginRequired, userController.index); // Nem esse
+router.get('/', loginRequired, userController.index); // Nem esse
 router.get('/:id', loginRequired, userController.show);
 router.put('/', loginRequired, userController.update);
 router.delete('/', loginRequired, userController.delete);
