6d1985e9b4fbcc742d0d59230e83a735d5d7846d
Fabricio de Freitas <fabricioffa08@gmail.com>
2021-10-01 10:59:23 -0300

    CRUD de user completa

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -15,7 +15,7 @@ class UserController {
 
   async index(req, res) {
     try {
-      const users = await User.findall();
+      const users = await User.findAll();
       res.json(users);
     } catch (e) {
       res.json(null);
@@ -23,9 +23,68 @@ class UserController {
   }
   //  SHOW
 
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
   //  UPDATE
 
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.json({
+          errors: ['O usuário não existe'],
+        });
+      }
+
+      const userUpdated = await user.update(req.body);
+
+      return res.json(userUpdated);
+    } catch (e) {
+      return res.status(400)
+        .json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
   //  DELETE
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.json({
+          errors: ['O usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.send('Usuário removido com sucesso');
+    } catch (e) {
+      return res.status(400)
+        .json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
 
   //  SHOW
 }
