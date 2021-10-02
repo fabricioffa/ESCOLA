d1082b3703923ebaf4f2b58dab8ac145fcc2ebef
Fabricio de Freitas <fabricioffa08@gmail.com>
2021-10-02 08:40:38 -0300

    CRUD de alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,7 +5,91 @@ class AlunoController {
     try {
       const alunos = await Aluno.findAll();
       return res.json(alunos);
-    } catch (e) { return console.error(e); }
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.json({
+          errors: ['Missing ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(req.params.id);
+
+      if (!aluno) {
+        return res.json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      const alunoUpdated = await aluno.update(req.body);
+
+      return res.json(alunoUpdated);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.json({ errors: ['Missing ID'] });
+      }
+
+      const aluno = await Aluno.findByPk(req.params.id);
+
+      if (!aluno) {
+        return res.json({ errors: ['Aluno não existe'] });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.json({ errors: ['Missing ID'] });
+      }
+
+      const aluno = await Aluno.findByPk(req.params.id);
+
+      if (!aluno) {
+        return res.json({ errors: ['Aluno não existe'] });
+      }
+
+      await aluno.destroy();
+
+      return res.json('Successfuly deleted');
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
   }
 }
 
--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -3,12 +3,65 @@ import Sequelize, { Model } from 'sequelize';
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [2, 255],
+            msg: 'Sobrenome deve ter entre 2 e 255 caracteres.',
+          },
+        },
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [5, 255],
+            msg: 'Sobrenome deve ter entre 5 e 255 caracteres.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Already registered e-mail.',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Invalid e-mail.',
+          },
+        },
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          isInt: {
+            msg: 'Must be integer.',
+          },
+        },
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        validate: {
+          isFloat: {
+            msg: 'Must be integer or float.',
+          },
+        },
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        validate: {
+          isFloat: {
+            msg: 'Must be integer or float.',
+          },
+        },
+      },
     }, {
       sequelize,
     });
