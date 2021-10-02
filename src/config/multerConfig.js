d8c92114012257cb3e62fa434e82b840d41ea809
Fabricio de Freitas <fabricioffa08@gmail.com>
2021-10-02 10:06:18 -0300

    validating picture files

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,15 +1,22 @@
 import multer from 'multer';
 import { extname, resolve } from 'path';
 
-const random = () => Math.floor(Math.random() * 10000 + 10000);
+const randon = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
-  storage: multer.diskStorage({
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('File must be PBG or JPG.'));
+    }
+
+    return cb(null, true);
+  },
+  store: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
     },
     filename: (req, file, cb) => {
-      cb(null, `${Date.now()}_${random()}${extname(file.originalname)}}`);
+      cb(null, `${Date.now()}_${randon()}${extname(file.originalname)}}`);
     },
   }),
 };
