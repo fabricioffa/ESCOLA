e37433a30c6cb5634e1bfb5ce92f648489ef3d48
Fabricio de Freitas <fabricioffa08@gmail.com>
2021-10-04 09:40:41 -0300

    multer verified

--- src/routes/pictureRoutes.js
+++ src/routes/pictureRoutes.js
@@ -1,11 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
 import pictureController from '../controllers/PictureController';
-import multerConfig from '../config/multerConfig';
 
 const router = new Router();
-const upload = multer(multerConfig);
 
-router.post('/', upload.single('picture'), pictureController.store);
+router.post('/', pictureController.store);
 
 export default router;
